3ae00908abdd88c6d6d830bd884e2a8694a75329
WadihChekriMorkos <74407324+WadihChekriMorkos@users.noreply.github.com>
2021-08-30 18:27:33 +0300

    Update header.php

--- header/header.php
+++ header/header.php
@@ -5,7 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../header/headerStyle.css"/>
+    <link rel="stylesheet" href="../header/headerStyle.css"/>
+    
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    
     <script src="../jquery-3.5.1.min.js"></script>
 </head>
 <body>
@@ -19,7 +22,7 @@
                 <li><a href="#">About Us</a></li>
                 <li><a href="#">Contact Us</a></li>
                 <hr>
-                <li class="icons-mobile"><a href="#"><i class="fas fa-user-circle"></i><span>Register/Login</span></a></li>
+                <li class="icons-mobile"><a href="../register_login/register.php"><i class="fas fa-user-circle"></i><span>Register/Login</span></a></li>
                 <li class="icons-mobile"><a href="#"><i class="fas fa-shopping-cart"></i><span>Cart</span></a></li>
             </ul>
 
@@ -28,8 +31,29 @@
                 <form method="POST">    
                 <td><input type="text" placeholder="Search..."/></td>
                 <td><button><i class="fas fa-search"></i>&nbsp;&nbsp;<span id="searchText"></span></button></td> 
-                <td class="icon-style"><a href="../register_login/login.php"><i class="fas fa-user-circle"></i><span>Register/Login</span></a></td>
-                <td class="icon-style"><a href="#"><i class="fas fa-shopping-cart"></i><span>Cart</span>
+                <td class="icon-style">
+                    
+                <?php 
+                session_start();
+                if(isset($_SESSION["clientName"]) && !empty($_SESSION["clientName"])){
+                    echo "<i class=\"fas fa-user-circle\"></i><span>".$_SESSION["clientName"]."</span>";
+                    echo "<td class=\"icon-style\"><a><i class=\"fas fa-shopping-cart\"></i><span>Cart</span>";
+                }
+                else if(isset($_SESSION["companyName"]) && !empty($_SESSION["companyName"])){
+                    echo "<i class=\"fas fa-building\"></i><span>".$_SESSION["companyName"]."</span>";
+                    echo "<td class=\"icon-style\"><a><i class=\"fas fa-shopping-cart\"></i><span>Cart</span>";
+                }
+
+
+
+
+                else{
+                    echo "<a href=\"../register_login/register.php\"><i class=\"fas fa-user-circle icon-style\"></i>
+                    <span>Register/Login</span></a>";    
+                echo "</td>";
+                }
+                ?>                   
+               
                 </tr>       
             </table>
 
