1e0469beb5a662a0244cbae97ea59f7af866b043
WadihChekriMorkos <74407324+WadihChekriMorkos@users.noreply.github.com>
2021-08-30 18:29:35 +0300

    Update login.php

--- register_login/login.php
+++ register_login/login.php
@@ -5,20 +5,34 @@
 
 <?php
 include "../header/header.php";
+include "login_handler.php";
+
 ?>
 <div class="container-login">
     <h1>Log in</h1>
-    <form method="POST">
+    <?php 
+    if(!empty($error))
+    echo "<div class=\"error\">$error</div>";
+    ?>
+    <form method="POST" action="login_handler.php">
         <div>
             <span>Email</span>
-            <span><input type="email" class="adjust-inp"/></span>
+            <span><input type="email" name="email" class="adjust-inp"/></span>
         </div>
         
         <div>
             <span>Password</span>
-            <span><input type="password" class="adjust-inp"/></span>
+            <span><input type="password" name="pass" class="adjust-inp"/></span>
         </div>
-        
+        <div>
+            <span>
+                <input type="radio" name="accountType" id="cust"  value="customer"/>  Customer
+                <input type="radio" name="accountType" id="comp" value="company"/>   Company
+             </span>
+        </div>
+        <div>
+            <span>Don't have an account?  <a href="register.php">Register an account</a>
+        </div>
         <div>
             <input type="submit" value="Log In"/>
         </div>
