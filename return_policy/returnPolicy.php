b76f0eeb9bf9bbc739d8955b84482f657547a400
WadihChekriMorkos <74407324+WadihChekriMorkos@users.noreply.github.com>
2021-09-15 19:10:31 +0300

    Add files via upload

---
+++ return_policy/returnPolicy.php
@@ -0,0 +1,22 @@
+<?php
+include "../header/header.php";
+?>
+<head>
+    <link rel="stylesheet" href="returnPolicyStyle.css">
+</head>
+<div class="policie-container">
+<h3>Returns Policy</h3>
+<ul>
+<li>You may return most new, unopened items within 30 days of delivery for a full refund. We'll also pay the return shipping costs if the return is a result of our error (you received an incorrect or defective item, etc.).</li>
+<li>You should expect to receive your refund within four weeks of giving your package to the return shipper, however, in many cases you will receive a refund more quickly. This time period includes the transit time for us to receive your return from the shipper (5 to 10 business days), the time it takes us to process your return once we receive it (3 to 5 business days), and the time it takes your bank to process our refund request (5 to 10 business days).</li>
+<li>If you need to return an item, simply login to your account, contact us once we've received and processed the returned item.</li>
+</ul>
+<h3>Shipping</h3>
+<ul>
+<li>When you place an order, we will estimate shipping and delivery dates for you based on the availability of your items and the shipping options you choose. Depending on the shipping provider you choose, shipping date estimates may appear on the shipping quotes page.</li>
+<li>Please also note that the shipping rates for many items we sell are weight-based. The weight of any such item can be found on its detail page. To reflect the policies of the shipping companies we use, all weights will be rounded up to the next full pound.</li>
+</ul>
+</div>
+<?php
+include "../footer/footer.php";
+?>
