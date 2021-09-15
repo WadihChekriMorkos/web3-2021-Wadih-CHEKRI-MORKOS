a1c9476cf83ad021a43c73347385190be871e8a7
WadihChekriMorkos <74407324+WadihChekriMorkos@users.noreply.github.com>
2021-09-15 19:11:26 +0300

    Update header.php

--- header/header.php
+++ header/header.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../header/headerStyle.css"/>
-    
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     
     <script src="../jquery-3.5.1.min.js"></script>
@@ -14,8 +13,8 @@
     <div class="container">
         <div class="client-options">
             <ul>
-                <li><a href="#">View my profile</a></li>
-                <li><a href="../register_login/logout.php">Logout</a></li>
+                <a href="../user_profile/profile.php"><li>View my profile</li></a>
+                <a href="../register_login/logout.php"><li>Logout</li></a>
         </div>
     
         <div class="navbar">
@@ -36,8 +35,7 @@
                 ?>
                  </ul>
                 </li>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Contact Us</a></li>
+            <li><a href="../contact/contactUs.php">Contact Us</a></li>
                 <hr>
                 <li class="icons-mobile"><a href="../register_login/register.php"><i class="fas fa-user-circle"></i><span>Register/Login</span></a></li>
             </ul>
@@ -52,11 +50,11 @@
                 session_start();
                 if(isset($_SESSION["clientName"]) && !empty($_SESSION["clientName"])){
                     echo "<i class=\"fas fa-user-circle icon-click\"></i><span class=\"icon-click\">".$_SESSION["clientName"]."</span></td>";
-                    echo "<td class=\"icon-style\"><a><i class=\"fas fa-shopping-cart\"></i><span>Cart</span>";
+                    echo "<td class=\"icon-style\"><i class=\"fas fa-shopping-cart\"></i><span><a href=\"../cart/cart.php\">Cart</a></span></td>";
                 }
                 else if(isset($_SESSION["companyName"]) && !empty($_SESSION["companyName"])){
                     echo "<i class=\"fas fa-building icon-click\"></i><span class=\"icon-click\">".$_SESSION["companyName"]."</span></td>";
-                    echo "<td class=\"icon-style\"><a><i class=\"fas fa-shopping-cart\"></i><span>Cart</span>";
+                    echo "<td class=\"icon-style\"><i class=\"fas fa-shopping-cart\"></i><span><a href=\"../cart/cart.php\">Cart</a></span></td>";
                 }
                 else{
                     echo "<a href=\"../register_login/register.php\"><i class=\"fas fa-user-circle icon-style\"></i>
