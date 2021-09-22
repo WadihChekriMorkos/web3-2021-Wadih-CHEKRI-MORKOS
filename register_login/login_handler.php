b400ba2f2dcc268611ff1bd0fd195605e81f75fc
WadihChekriMorkos <74407324+WadihChekriMorkos@users.noreply.github.com>
2021-09-22 19:45:10 +0300

    Update login_handler.php

--- register_login/login_handler.php
+++ register_login/login_handler.php
@@ -1,15 +1,14 @@
 <?php
 include "../db_con/connection.php";
+session_start();
 if(isset($_POST["accountType"]) && !empty($_POST["accountType"])){
 $checkbox=$_POST["accountType"];
 $error="";
 $checker=true;
+if($checkbox=="customer"){
     if(isset($_POST["email"]) && !empty($_POST["email"]) && isset($_POST["pass"]) && !empty($_POST["pass"])){
-          
         $email=validate_input($_POST["email"]);
-          $pass=validate_input($_POST["pass"]);
-       
-        
+        $pass=validate_input($_POST["pass"]);
        if(!preg_match("/^\w+@\w+[.]\w+$/",$email)){
         $error.="email contains errors<br>";
         $checker=false;
@@ -19,38 +18,67 @@ $checker=true;
         $error.="password length must be 8 or above<br>";
         $checker=false;
         }
-
-        if($checker==true && $checkbox=="company"){
-            $pass=md5($pass);
-            $requete="select * from company where email='$email' and password='$pass'";
-           $result= mysqli_query($con,$requete);
+        if($checker==true){
+        $requete="select clientId,firstName,password from clients where email='$email'";
+        $result=mysqli_query($con,$requete);
             if(mysqli_num_rows($result)==0){
-                $error="This account does not exists";
+                $error="Client does not exists";
             }
             else{
-               $resultat=mysqli_fetch_assoc($result);
-               $_SESSION["companyName"]=$resultat["companyName"];
-               header("Location:../home/home.php");
+                $row=mysqli_fetch_assoc($result);
+                if(!password_verify($pass,$row["password"])){
+                    $error="Incorrect Email/Password combination";
+                }
+                else{
+                    $_SESSION["clientName"]=$row["firstName"];
+                    $_SESSION["clientId"]=$row["clientId"];
+                    header("Location:../home/home.php");
+                }
             }
-
-        }
-        else if($checker==true && $checkbox=="customer"){
-            $pass=md5($pass);
-            $requete="select * from clients where email='$email' and password='$pass'";
-           $result= mysqli_query($con,$requete);
-            if(mysqli_num_rows($result)==0){
-                $error="This account does not exists";
-            }
-            else{
-                $resultat=mysqli_fetch_assoc($result);
-                $_SESSION["clientName"]=$resultat["firstName"];
-                header("Location:../home/home.php");
-            } 
-        }
+        }  
     }
-    
+    if(!empty($error)){
+        header("Location:login.php?error=$error");
+    }
+}
 
 
+if($checkbox=="company"){
+    if(isset($_POST["email"]) && !empty($_POST["email"]) && isset($_POST["pass"]) && !empty($_POST["pass"])){
+        $email=validate_input($_POST["email"]);
+        $pass=validate_input($_POST["pass"]);
+       if(!preg_match("/^\w+@\w+[.]\w+$/",$email)){
+        $error.="email contains errors<br>";
+        $checker=false;
+        }
+        
+       if(!preg_match("/^\w{8,}$/",$pass)){
+        $error.="password length must be 8 or above<br>";
+        $checker=false;
+        }
+        if($checker==true){
+        $requete="select companyId,companyName,password from company where email='$email'";
+        $result=mysqli_query($con,$requete);
+            if(mysqli_num_rows($result)==0){
+                $error="Company does not exists";
+            }
+            else{
+                $row=mysqli_fetch_assoc($result);
+                if(!password_verify($pass,$row["password"])){
+                    $error="Incorrect Email/Password combination";
+                }
+                else{
+                    $_SESSION["companyName"]=$row["companyName"];
+                    $_SESSION["companyId"]=$row["companyId"];
+                    header("Location:../home/home.php");
+                }
+            }
+        }  
+    }
+    if(!empty($error)){
+        header("Location:login.php?error=$error");
+    }
+}
 }
 function validate_input($data) {
     $data = trim($data);
