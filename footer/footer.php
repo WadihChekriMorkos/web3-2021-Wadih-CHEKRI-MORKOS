445224c420f1ade82a93c13652a940f051feaa6f
WadihChekriMorkos <74407324+WadihChekriMorkos@users.noreply.github.com>
2021-09-30 15:23:56 +0300

    Update footer.php

--- footer/footer.php
+++ footer/footer.php
@@ -33,9 +33,6 @@
                 <li><a href="../home/home.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right" viewBox="0 0 16 16">
   <path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z"/>
 </svg>Home</a>
-                <li><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right" viewBox="0 0 16 16">
-  <path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z"/>
-</svg>Products Categories</a>
             </ul>
         </div>
         <div class="third">
@@ -85,13 +82,13 @@
             });
             
             $("#comp").click(function(){
-                $(".customer").hide();
+                $(".client").hide();
                 $(".company").show();
             });
             
             
             $("#cust").click(function(){
-                $(".customer").show();
+                $(".cline").show();
                 $(".company").hide();
             });
     </script>    
