78f4691dc4f7e3c1fbc6e7e095aaa486b690d5ba
WadihChekriMorkos <74407324+WadihChekriMorkos@users.noreply.github.com>
2021-09-30 15:27:17 +0300

    Update header.php

--- header/header.php
+++ header/header.php
@@ -1,13 +1,15 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../header/headerStyle.css"/>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
-    
-    <script src="../jquery-3.5.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../header/headerStyle.css"/>
 </head>
 <body>
     <div class="container">
@@ -27,27 +29,43 @@
                     <ul class="categorie-list">
                 <?php 
                     include "../db_con/connection.php";
-                    $allCategories="select categorieName from categories";
+                    $allCategories="select categoryName,categoryId from categories";
                     $result=mysqli_query($con,$allCategories);
                     while($row=mysqli_fetch_assoc($result)){
-                        echo "<li>".$row["categorieName"]."</li>";
+                        echo "<a href=\"../products/viewProducts.php?i=".$row["categoryId"]."\"><li>".$row["categoryName"]."</li></a>";
                     }
                 ?>
                  </ul>
                 </li>
             <li><a href="../contact/contactUs.php">Contact Us</a></li>
+            <?php
+                if(isset($_SESSION["companyId"]) && !empty($_SESSION["companyId"])){
+                    echo "<li><a href=\"../add_product/addProduct.php\">Add a product</li>";
+                    echo "<li><a href=\"../products/companyViewProduct.php\">View my products</li>";
+                }
+            ?>
                 <hr>
-                <li class="icons-mobile"><a href="../register_login/register.php"><i class="fas fa-user-circle"></i><span>Register/Login</span></a></li>
             </ul>
-
-            <table>
+                <div class="mob-btn">
+                    <?php
+                    if(isset($_SESSION["clientId"]) && !empty($_SESSION["clientId"])
+                    || isset($_SESSION["companyId"]) && !empty($_SESSION["companyId"])){
+                    echo "<div><a href=\"../user_profile/profile.php\">View my account</a></div>";
+                    echo "<div><a href=\"../cart/cart.php\">My cart</a></div>";
+                    echo "<div><a href=\"../register_login/logout.php\">Log out</a></div>";
+                    }
+                    else{
+                        echo "<div><a href=\"../register_login/register.php\">Register a new account</a></div>";
+                    }
+                    ?>
+                </div>
+            <table class="tab">
                 <tr>
-                <form method="POST">    
-                <td><input type="text" placeholder="Search..."/></td>
-                <td><button><i class="fas fa-search"></i>&nbsp;&nbsp;<span id="searchText"></span></button></td> 
+                <form method="POST" action="../search/searchPage.php">    
+                <td><input type="text" name="search" placeholder="Search..."/></td>
+                <td><button type="submit"><i class="fas fa-search"></i>&nbsp;&nbsp;<span id="searchText"></span></button></td> 
                 <td class="icon-style">
-                <?php 
-                session_start();
+                <?php
                 if(isset($_SESSION["clientName"]) && !empty($_SESSION["clientName"])){
                     echo "<i class=\"fas fa-user-circle icon-click\"></i><span class=\"icon-click\">".$_SESSION["clientName"]."</span></td>";
                     echo "<td class=\"icon-style\"><i class=\"fas fa-shopping-cart\"></i><span><a href=\"../cart/cart.php\">Cart</a></span></td>";
@@ -61,7 +79,8 @@
                     <span class=\"reg-log\">Register/Login</span></a>";    
                 echo "</td>";
                 }
-               ?>              
+               ?>    
+               </form>          
                 </tr>       
             </table>
             <div class="mobile-menu">
@@ -98,15 +117,16 @@
             /*MOBILE PARTS*/ 
            $(".mobile-menu").click(function(){
                $(".navbar ul").toggle();
-               $("table").toggle();
+               $(".tab").toggle();
+               $(".mob-btn").toggle();
            });
            
            $( window ).resize(function() {
         if($(window).width()>=1100){
             $(".navbar ul").show();
-            $("table").show();
+            $(".tab").show();
         }
-        if($(window).width()<900){
+        if($(window).width()<700){
             $(".categorie-list").css("position","static");
         }
         else{
